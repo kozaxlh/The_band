9bca3f8c81fa9bc1bf2f0c634f65b18691c4072a
kozaxlh <83365301+kozaxlh@users.noreply.github.com>
2021-10-03 01:18:57 +0700

    responsive and fixed

--- src/script.js
+++ src/script.js
@@ -1,8 +1,38 @@
+const mobileMenu = document.querySelector('#mobile-menu')
+const header = document.querySelector('#js-header')
+const menuItems = document.querySelectorAll('.nav-link li a[href*="#"]')
 const buyButtons = document.querySelectorAll('.js-buy-ticket');
 const closeButtons = document.querySelectorAll('.js-close-button');
 const modal = document.querySelector('.modal');
-const modalContainer = document.querySelector('.modal-container')
+const modalContainer = document.querySelector('.modal-container');
 
+let headerHeight = header.clientHeight;
+
+// Show/remove menu
+mobileMenu.onclick = () => {
+    let isClose = header.clientHeight === headerHeight
+    if (isClose)
+        header.style.height = 'auto'
+    else
+        header.style.height = null
+}
+
+//Auto close menu when click <a>
+for (const menuItem of menuItems) {
+    const isParentMenu = menuItem.nextElementSibling && menuItem.nextElementSibling.classList.contains('sub-nav')
+
+    menuItem.onclick = (event) => {
+        if (isParentMenu) {
+            event.preventDefault();
+        }
+        else {
+            header.style.height = null
+        }
+    }
+}
+
+
+//  Modal show
 function showBuyTicket() {
     modal.classList.add('open');
 }
@@ -13,16 +43,17 @@ function removeBuyTicket() {
 
 // Open modal
 for (const buyButton of buyButtons) {
-    buyButton.addEventListener('click',showBuyTicket)
+    buyButton.addEventListener('click', showBuyTicket)
 }
 
 // Close modal
-for(const closeButton of closeButtons) {
-    closeButton.addEventListener('click',removeBuyTicket)
+for (const closeButton of closeButtons) {
+    closeButton.addEventListener('click', removeBuyTicket)
 }
 
-modal.addEventListener('click',removeBuyTicket)
+modal.addEventListener('click', removeBuyTicket)
 
 modalContainer.addEventListener("click", (event) => {
     event.stopPropagation()
 })
+
